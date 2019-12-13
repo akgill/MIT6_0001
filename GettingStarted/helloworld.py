def188fbc2c3156a13e65e80c4097f4eb2953ae6
Amandeep Boyer <amandeep.boyer@gmail.com>
2019-12-12 22:34:42 -0700

    GettingStarted and PS0 assignments

---
+++ GettingStarted/helloworld.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2019/12/12
+
+@author: Amandeep Boyer
+"""
+
+print("Hello world")
+print("I like 6.001!")
---
+++ PS0/ps0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 12 21:48:55 2019
+
+@author: akgill
+"""
+from math import log
+
+x = int(input("Enter number x "))
+y = int(input("Enter number y "))
+print("\nX**y = ", x**y, "\n")
+print("log(x) = ", int(log(x, 2)), "\n")
